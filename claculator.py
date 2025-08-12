0c10bf7419992894946e0f012deabd28f1a6e933
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-12 07:38:07 +0430

    leabel & mainloop.

--- claculator.py
+++ claculator.py
@@ -6,3 +6,8 @@ root.title("Calculator")
 root.geometry("570x600+100200")
 root.resizable(False, False)
 root.configure(bg="#17161b")
+
+label_result = Label(root, width=25, height=2, text="", font=("arial", 30))
+label_result.pack()
+
+root.mainloop()
