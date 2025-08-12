2f3166cf967cc4e2bbd3cb64d3b736c293bbdecf
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-12 07:41:19 +0430

    Add C button.

--- claculator.py
+++ claculator.py
@@ -10,4 +10,6 @@ root.configure(bg="#17161b")
 label_result = Label(root, width=25, height=2, text="", font=("arial", 30))
 label_result.pack()
 
+Button(root, text="C", width=2, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#3697f5").place(x=10, y=100)
+
 root.mainloop()
