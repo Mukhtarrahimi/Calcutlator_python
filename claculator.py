22dfa10ac2917b18227199bc5c1d4d0081ab39a5
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-12 07:44:29 +0430

    Add Remainder button.

--- claculator.py
+++ claculator.py
@@ -11,6 +11,7 @@ label_result = Label(root, width=25, height=2, text="", font=("arial", 30))
 label_result.pack()
 
 Button(root, text="C", width=2, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#3697f5").place(x=10, y=100)
-Button(root, text="/", width=2, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36").place(x=10, y=100)
+Button(root, text="/", width=2, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36").place(x=150, y=100)
+Button(root, text="%", width=2, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36").place(x=290, y=100)
 
 root.mainloop()
